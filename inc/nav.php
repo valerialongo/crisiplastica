32358ec09afe7a4de83d3ca17a6721e54db0985f
valerialongo <valerialongom@gmail.com>
2017-10-05 01:09:09 +0200

    Add files via upload

---
+++ inc/nav.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-toggleable-md navbar-light bg-faded">
+  <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <a class="navbar-brand" href="#">RegAndLogin</a>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#reg">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+     
+    </ul>
+    <form class="form-inline my-2 my-lg-0"> 
+        <input class="form-control mr-sm-2" type="email" name="email" placeholder="Email" required>
+        <input class="form-control mr-sm-2" type="password" name="password" placeholder="pwd" required>
+      <input class="form-control mr-sm-2" type="text" placeholder="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="sendLogin">Login</button>
+    </form>
+  </div>
+</nav>
